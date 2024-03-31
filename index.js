62514c346746280b9a9d2355e538378ffc61f616
silentshaikh <sheikhabdulmoiz283@gmail.com>
2024-03-31 06:23:39 +0500

    update file

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
-import inquirer from 'inquirer';
+import inquirer from "inquirer";
 let todoList = [];
 let count = 0;
 let isCondition = true;
 while (isCondition) {
     let addTodo = await inquirer.prompt([
-        { name: 'todo', type: 'input', message: 'Enter Your Todo' },
-        { name: 'moreTodos', type: 'confirm', message: 'Do you want to add more Todos', default: "false" }
+        { name: "todo", type: "input", message: "Enter Your Todo" },
+        {
+            name: "moreTodos",
+            type: "confirm",
+            message: "Do you want to add more Todos",
+            default: "false",
+        },
+        {
+            name: "confirmRemoveTodo",
+            type: "confirm",
+            message: "Do you want to remove a Todo",
+            default: "false",
+        },
     ]);
     if (addTodo.todo === "") {
         console.log("Please add a todo");
     }
     else {
-        todoList.push({ id: ++count, todo: addTodo.todo });
+        todoList.push({ id: count++, todo: addTodo.todo });
     }
     isCondition = addTodo.moreTodos;
     console.log(todoList);
+    let isRemTodo = addTodo.confirmRemoveTodo;
+    if (isRemTodo) {
+        let remTodo = await inquirer.prompt([
+            {
+                name: "removeTodo",
+                type: "number",
+                message: "Enter a id for remove a Todo",
+            },
+        ]);
+        const removeTodos = (todoId) => {
+            let delTodo = todoList.filter((e) => {
+                return e.id !== todoId;
+            });
+            todoList = delTodo;
+        };
+        let findId = todoList.find((e) => e.id === remTodo.removeTodo);
+        if (findId) {
+            removeTodos(remTodo.removeTodo);
+            console.log("After Remove a Todo");
+            console.log(todoList);
+        }
+        else {
+            console.log(`The ID ${remTodo.removeTodo} is not available`);
+        }
+    }
 }
--- index.ts
+++ index.ts
@@ -1,22 +1,61 @@
-import inquirer from 'inquirer';
+import inquirer from "inquirer";
 interface Todo{
-    id:number,
-    todo:string
+  id: number;
+  todo: string;
 }
-let todoList:Todo[] = [];
-let count:number = 0;
-let isCondition:boolean = true;
-while(isCondition){
-let addTodo = await inquirer.prompt([
-    {name:'todo',type:'input',message:'Enter Your Todo'},
-    {name:'moreTodos',type:'confirm',message:'Do you want to add more Todos',default:"false"}
-]);
-    if(addTodo.todo === ""){
-        console.log("Please add a todo");
+let todoList: Todo[] = [];
+let count: number = 0;
+let isCondition: boolean = true;
+// Add a Todo
+while (isCondition) {
+  let addTodo = await inquirer.prompt([
+    { name: "todo", type: "input", message: "Enter Your Todo" },
+    {
+      name: "moreTodos",
+      type: "confirm",
+      message: "Do you want to add more Todos",
+      default: "false",
+    },
+    {
+      name: "confirmRemoveTodo",
+      type: "confirm",
+      message: "Do you want to remove a Todo",
+      default: "false",
+    },
+  ]);
+  // Confirm for Todo is null or not null
+  if (addTodo.todo === "") {
+    console.log("Please add a todo");
+  } else {
+    todoList.push({ id: count++, todo: addTodo.todo});
+  }
+  isCondition = addTodo.moreTodos;
+  console.log(todoList);
+  // Remove a Todo
+  let isRemTodo = addTodo.confirmRemoveTodo;
+  if (isRemTodo) {
+    let remTodo = await inquirer.prompt([
+      {
+        name: "removeTodo",
+        type: "number",
+        message: "Enter a id for remove a Todo",
+      },
+    ]);
+    // Filterable Todo
+    const removeTodos = (todoId:number) => {
+      let delTodo:Todo[] = todoList.filter((e) => {
+        return e.id !== todoId  
+      })
+      todoList =  delTodo;
     }
-    else{
-        todoList.push({id:++count,todo:addTodo.todo});
-    }
-    isCondition = addTodo.moreTodos;
+    // Find Todo ID
+    let findId:Todo | undefined = todoList.find((e) => e.id === remTodo.removeTodo) 
+   if(findId){
+    removeTodos(remTodo.removeTodo);
+    console.log("After Remove a Todo");
     console.log(todoList);
+   }else{
+    console.log(`The ID ${remTodo.removeTodo} is not available`);
+   }
+  }
 }
